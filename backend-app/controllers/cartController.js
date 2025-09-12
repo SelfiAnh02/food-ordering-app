d3fcc7aee80555ef6a9f201ee6a8def23e0f860c
Selfianh02 <selfianh020304@gmail.com>
2025-09-12 16:10:30 +0700

    fix my cartController

--- backend-app/controllers/cartController.js
+++ backend-app/controllers/cartController.js
@@ -3,7 +3,7 @@ import userModel from "../models/userModel.js";
 // add item to user cart
 const addToCart = async (req, res) => {
     try {
-        let userData = await userModel.findById(req.body.userId);
+        let userData = await userModel.findById(req.user.id);
         let cartData = await userData.cartData;
         if (!cartData[req.body.productId]) {
             cartData[req.body.productId] = 1;
@@ -12,40 +12,39 @@ const addToCart = async (req, res) => {
         }
 
         await userModel.findByIdAndUpdate(
-            req.body.userId,
+            req.user.id,
             { cartData }
         );  
         res.status(200).json({success: true, message: "Item added to cart successfully"});
     } catch (error) {
         console.log(error);
-        res.status(500).json({success: false, message: "Internal Server Error"});
+        res.status(500).json({success: false, message: "error lagi deh"});
     }
-
 }
 
 // remove item from user cart
 const removeFromCart = async (req, res) => {
     try {
-        let userData = await userModel.findById(req.body.userId);
+        let userData = await userModel.findById(req.user.id);
         let cartData = userData.cartData;
         if (cartData[req.body.productId]>0) {
             cartData[req.body.productId] -= 1;
         }
         await userModel.findByIdAndUpdate(
-            req.body.userId,
+            req.user.id,
             { cartData }
         );  
         res.status(200).json({success: true, message: "Item removed from cart successfully"});
     } catch (error) {
         console.log(error);
-        res.status(500).json({success: false, message: "Internal Server Error"});
+        res.status(500).json({success: false, message: "Error lagi broow"});
     }
 }
     
 // fetch user cart data 
 const getCart = async (req, res) => {
     try {
-        const user = await userModel.findById(req.body.userId);
+        const user = await userModel.findById(req.user.id);
 
         if (!user) {
             return res.status(404).json({ success: false, message: "User not found" });
@@ -57,7 +56,7 @@ const getCart = async (req, res) => {
         });
     } catch (error) {
         console.log(error);
-        res.status(500).json({ success: false, message: "Internal Server Error" });
+        res.status(500).json({ success: false, message: "Error lagi broow" });
     }
 };
 
