073e15a5dea01bb70298f7fadc4c0c1fb0a0a23b
Selfianh02 <selfianh020304@gmail.com>
2025-09-12 16:09:09 +0700

    fix my auth.js

--- backend-app/middleware/auth.js
+++ backend-app/middleware/auth.js
@@ -2,19 +2,29 @@ import jwt from "jsonwebtoken";
 import userModel from "../models/userModel.js";
 
 const authMiddleware = async (req, res, next) => {
-  const {token} = req.headers;
+  const authHeader = req.headers["authorization"];
+  if (!authHeader) {
+    return res.status(401).json({ message: "Not Authorized Login Again" });
+  }
+
+  // ambil token setelah "Bearer "
+  const token = authHeader.split(" ")[1];
   if (!token) {
     return res.status(401).json({ message: "Not Authorized Login Again" });
   }
+
   try {
     const token_decode = jwt.verify(token, process.env.JWT_SECRET);
-    req.body.userId = token_decode.id;
+    req.user = token_decode;
     next();
   } catch (error) {
     console.log(error);
     return res.status(401).json({ message: "Not Authorized Login Again" });
   }
 };
+
+
+
 
 // role check
 const authorize = (...roles) => {
